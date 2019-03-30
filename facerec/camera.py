f25b2ac8f7df71fb5e9d0433ce562e9fbe3df7d9
Matias Szylkowski <mszylkowski@hotmail.com>
2019-03-30 03:18:31 -0400

    working face recognition

--- facerec/camera.py
+++ facerec/camera.py
@@ -1,13 +1,36 @@
 import cv2
-import sys
+import requests
+import json
+import base64
+import numpy as np
 
 cap = cv2.VideoCapture(0)
-
-# Get user supplied values
 cascPath = "haarcascade_frontalface_default.xml"
-
-# Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
+
+def sendImage(image):
+    subscription_key = '87d9518f24ca4658a98b2ad80dc2fb57'
+
+    face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/identify'
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': subscription_key
+    }
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        # 'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+        'returnFaceAttributes': 'age',
+    }
+
+    retval, buffer = cv2.imencode('.jpg', image)
+    buffer = np.array(buffer).reshape((len(buffer)))
+
+    #response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+    response = requests.post(face_api_url, params=params, headers=headers, data=bytes(buffer.tolist()))
+    print(json.dumps(response.json()))
+
+currImagesCenters = []
 
 while(True):
     # Capture frame-by-frame
@@ -25,12 +48,17 @@ while(True):
     )
 
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    if len(faces) > 0:
+        # for (x, y, w, h) in faces:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("Faces found", image)
+        sendImage(image)
+        cap.release()
+        cv2.waitKey()
+        break
 
     cv2.imshow("Faces found", image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
         break
-
-cap.release()
-cv2.destroyAllWindows()
