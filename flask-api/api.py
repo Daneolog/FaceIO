8e85530897d868775f94689f78a096b7d0ceff2e
Davi Nakajima An <dan36@gatech.edu>
2019-03-30 03:42:14 -0400

    all routes implemented

--- flask-api/api.py
+++ flask-api/api.py
@@ -17,46 +17,67 @@ def after_request(response):
 pic0 = "https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/1926287_542834289163885_1403268875_o.jpg?_nc_cat=106&_nc_ht=scontent-atl3-1.xx&oh=a87047ab6817fe144e3e819b311b5d31&oe=5D4EB408"
 pic1 = "https://media.licdn.com/dms/image/C5603AQG6BF4IsM7Eaw/profile-displayphoto-shrink_200_200/0?e=1559174400&v=beta&t=H0VDKP5cQA4zf-gdFLIhuOoNf2ANBi3AKCBF2JG2i-o"
 person0 =  {
-                            "pfp" : "oi",
-                            "name" : "Dominic Decoco",
-                            "phonesOwned" : [{"image" : pic0, "name" : "Samsung S9"}],
-                            "interests" : ["Samsung", "Android"],
-                            "phoneNo" : "4701929374",
-                            "plan" : {"type" : "prepaid", "price": 50, "data" : "unlimited", "msg" : "unlimited", "calls" : "unlimited"}
-                        }
+                "pfp" : "oi",
+                "name" : "Dominic Decoco",
+                "phonesOwned" : [{"image" : pic0, "name" : "Samsung S9"}],
+                "interests" : ["Samsung", "Android"],
+                "phoneNo" : "4701929374",
+                "plan" : {"type" : "prepaid", "price": 50, "data" : "unlimited", "msg" : "unlimited", "calls" : "unlimited"}
+            }
             
 
 person1 =  {
-                            "pfp" : "oi",
-                            "name" : "Gabigol",
-                            "phonesOwned" : [{"image" : pic1, "name" : "Samsung S9"}],
-                            "interests" : ["Samsung", "Android"],
-                            "phoneNo" : "4701929374",
-                            "plan" : {"type" : "prepaid", "price": 50, "data" : "unlimited", "msg" : "unlimited", "calls" : "unlimited"}
-                        }
-customers = {"fid0" : person0, "fid1" : person1} # faces : person_data
-all_customers = {}
+                "pfp" : "oi",
+                "name" : "Gabigol",
+                "phonesOwned" : [{"image" : pic1, "name" : "Samsung S9"}],
+                "interests" : ["Samsung", "Android"],
+                "phoneNo" : "4701929374",
+                "plan" : {"type" : "prepaid", "price": 50, "data" : "unlimited", "msg" : "unlimited", "calls" : "unlimited"}
+            }
 
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
+fid2tid = {
+    "fid0": "tid0",
+    "fid1": "tid1",
+    "fid2": None
+}
+tid2info = {"tid0" : person0, "tid1" : person1} # faces : person_data
+curr_fids = ["fid0", "fid1"]
 
 @app.route("/customers", methods=['GET'])
-def all_customers():
-    if request.method == 'GET':
-        return jsonify(customers)
-    else:
-        return "405: Restricted method"
+def allCustomers():
+    customers = {} 
+    for fid in curr_fids:
+        customers[fid] = tid2info[fid2tid[fid]]
+    return jsonify(customers)
 
 @app.route("/customers/<key>", methods=['GET', 'POST'])
-def customer(key):
+def specificCustomer(key):
     if request.method == 'GET':
-        return jsonify(customers[key])
+        if tid2info[fid] is None:
+            return jsonify({})
+        return jsonify(tid2info[fid2tid[key]])
     elif request.method == 'POST':
-        customers[key] = request.args.get('data', None)
+        tid2info[fid2tid[key]] = request.args.get('data', None)
     else:
         return "405: Restricted method"
 
+@app.route("/store/faces", methods=['GET'])
+def store_faces():
+    faces = {}
+    for fid in curr_fids:
+        if fid2tid[fid] is None:
+            faces[fid] = {"pfp": "", "name": "undefined", "isCustomer": False}
+        else:
+            cust = tid2info[fid2tid[fid]]
+            faces[fid] = {"pfp": cust["pfp"], "name": cust["name"], "isCustomer": True}
+    return jsonify(faces)
+
+@app.route("/store/faces/<fid>/assign/<tid>", methods=['GET'])
+def assign_face_to_tid(fid, tid):
+    if tid in tid2info:
+        fid2tid[key] = tid
+        return 200
+    return 400
+
 if __name__ == "__main__":
-    # app.run()
     app.run(host='0.0.0.0')
